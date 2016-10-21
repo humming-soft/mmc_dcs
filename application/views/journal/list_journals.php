5b1a9ef2fa479dad0a732965e72a0597a90f52f0
agailevictor <agailevictor@gmail.com>
2016-10-21 16:28:13 +0800

    Signed-off-by: agailevictor <agailevictor@gmail.com>

--- application/views/journal/list_journals.php
+++ application/views/journal/list_journals.php
@@ -59,77 +59,86 @@
     <div class="container">
         <!-- START EDIT CONTENT -->
         <?php if($this->session->flashdata('success')){ ?>
-            <div class="alert no-bg b-l-success b-l-3 b-t-gray b-r-gray b-b-gray" role="alert">
+            <!--<div class="alert no-bg b-l-success b-l-3 b-t-gray b-r-gray b-b-gray" role="alert">
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&#xD7;</span></button>
                 <strong class="text-white">Suscess!</strong> <span class="text-gray-lighter"><?php echo $this->session->flashdata('success'); ?>.</span>
-            </div>
-        <?php } ;?>
-        <?php  if($this->session->flashdata('error')){ ?>
+            </div>-->
+            <script type="text/javascript">
+                    swal({
+                        title: 'Success!',
+                        text: 'Journal succesfully added',
+                        type: 'success',
+                        allowEscapeKey: false,
+                        allowOutsideClick: false
+                    });
+            </script>
+            <?php } ;?>
+            <?php  if($this->session->flashdata('error')){ ?>
             <div class="alert no-bg b-l-warning b-l-3 b-t-gray b-r-gray b-b-gray" role="alert">
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&#xD7;</span></button>
                 <strong class="text-white">Faild!</strong> <span class="text-gray-lighter"><?php echo $this->session->flashdata('error'); ?>.</span>
             </div>
-        <?php } ;?>
-        <div class="row">
-            <div class="col-lg-12">
-                <table id="datatables-example" class="table table-striped table-bordered">
-                    <thead>
-                    <tr>
-                        <th  style="font-weight: 600; color: #2c97de">No</th>
-                        <th  style="font-weight: 600; color: #2c97de">Project Name</th>
-                        <th  style="font-weight: 600; color: #2c97de">Journal Name</th>
-                        <th  style="font-weight: 600; color: #2c97de">Type</th>
-                        <th  style="font-weight: 600; color: #2c97de">Action</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php $sno=1;
-                    foreach ($journal as $journal):?>
-                        <tr>
-                            <td class="text-white"><?php echo $sno; ?></td>
-                            <td class="text-white"><?php echo $journal['pjct_name']; ?></td>
-                            <td class="text-white"><?php echo $journal['journal_name']; ?></td>
-                            <td class="text-white"><?php echo $journal['journal_type_name']; ?></td>
-                            <td class="text-center v-a-m">
-                                <div class="btn-group" role="group" aria-label="...">
-                                    <a href="" data-toggle="modal" data-target="#myModal1" class="modaledit" data-journalId="<?php echo $journal['journal_master_id']; ?>" data-status="<?php echo $journal['journal_status']; ?>"  data-category="<?php echo $journal['journal_category_id'];?>" data-projectId="<?php echo $journal['pjct_master_id']; ?>" data-journalName="<?php echo $journal['journal_name'] ?>" data-typeId="<?php echo $journal['journal_type_id']; ?>" ><span class="glyphicon glyphicon-edit">&nbsp;</span></a>
-                                    <a href="javascript:;" id="showtoaster" class="modaldelete" data-journalId="<?php echo $journal['journal_master_id']; ?>" data-status="<?php echo $journal['journal_status']; ?>"  ><span class="glyphicon glyphicon-trash">&nbsp;</span></a>
-                                </div>
-                            </td>
-                        </tr>
-                        <?php
-                        $sno=$sno+1;
-                    endforeach; ?>
-                    </tbody>
-                </table>
-                <!-- END Zero Configuration -->
+            <?php } ;?>
+            <div class="row">
+                <div class="col-lg-12">
+                    <table id="datatables-example" class="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th  style="font-weight: 600; color: #2c97de">No</th>
+                                <th  style="font-weight: 600; color: #2c97de">Project Name</th>
+                                <th  style="font-weight: 600; color: #2c97de">Journal Name</th>
+                                <th  style="font-weight: 600; color: #2c97de">Type</th>
+                                <th  style="font-weight: 600; color: #2c97de">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $sno=1;
+                            foreach ($journal as $journal):?>
+                            <tr>
+                                <td class="text-white"><?php echo $sno; ?></td>
+                                <td class="text-white"><?php echo $journal['pjct_name']; ?></td>
+                                <td class="text-white"><?php echo $journal['journal_name']; ?></td>
+                                <td class="text-white"><?php echo $journal['journal_type_name']; ?></td>
+                                <td class="text-center v-a-m">
+                                    <div class="btn-group" role="group" aria-label="...">
+                                        <a href="" data-toggle="modal" data-target="#myModal1" class="modaledit" data-journalId="<?php echo $journal['journal_master_id']; ?>" data-status="<?php echo $journal['journal_status']; ?>"  data-category="<?php echo $journal['journal_category_id'];?>" data-projectId="<?php echo $journal['pjct_master_id']; ?>" data-journalName="<?php echo $journal['journal_name'] ?>" data-typeId="<?php echo $journal['journal_type_id']; ?>" ><span class="glyphicon glyphicon-edit">&nbsp;</span></a>
+                                        <a href="javascript:;" id="showtoaster" class="modaldelete" data-journalId="<?php echo $journal['journal_master_id']; ?>" data-status="<?php echo $journal['journal_status']; ?>"  ><span class="glyphicon glyphicon-trash">&nbsp;</span></a>
+                                    </div>
+                                </td>
+                            </tr>
+                            <?php
+                            $sno=$sno+1;
+                            endforeach; ?>
+                        </tbody>
+                    </table>
+                    <!-- END Zero Configuration -->
+                </div>
             </div>
+            <!-- END EDIT CONTENT -->
         </div>
-        <!-- END EDIT CONTENT -->
+
     </div>
 
-</div>
-
-<div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                <h4 class="modal-title" id="myModalLabel">Edit The Journal Details</h4>
-            </div>
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="form-horizontal">
-                            <?php echo form_open('journal/update_journal' , array('id' => 'validations'));?>
-                            <div class="form-group">
-                                <label for="name1" class="col-sm-3 control-label">Project Name</label>
-                                <div class="col-sm-9">
-                                    <input type="hidden" id="journalId" name ="journalId">
-                                    <select name="intPjtId" class="form-control" id="intPjtId"  >
-                                        <option value="-1">Select Project</option>
-                                        <?php
-                                        foreach ($records as $rec):?>
+    <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Edit The Journal Details</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="form-horizontal">
+                                <?php echo form_open('journal/update_journal' , array('id' => 'validations'));?>
+                                <div class="form-group">
+                                    <label for="name1" class="col-sm-3 control-label">Project Name</label>
+                                    <div class="col-sm-9">
+                                        <input type="hidden" id="journalId" name ="journalId">
+                                        <select name="intPjtId" class="form-control" id="intPjtId"  >
+                                            <option value="-1">Select Project</option>
+                                            <?php
+                                            foreach ($records as $rec):?>
                                             <option value="<?php echo $rec->pjct_master_id; ?>"><?php echo $rec->pjct_name; ?> </option>
                                         <?php  endforeach;?>
                                     </select>
@@ -150,39 +159,39 @@
                                         <option value="-1">Select Journal Type</option>
                                         <?php
                                         foreach ($journalType as $journaType):?>
-                                            <option value="<?php echo $journaType->journal_type_id; ?>"><?php echo $journaType->journal_type_name; ?> </option>
-                                        <?php  endforeach;?>
-                                    </select>
-                                    <?php echo form_error('name'); ?>
-                                </div>
-                            </div>
-                            <div class="form-group" id="selectshow">
-                                <label for="name1" class="col-sm-3 control-label">Journal Category</label>
-                                <div class="col-sm-9">
-                                    <select name="intCatId" class="form-control" id="intCatId">
-                                        <option value="-1">Select Journal Category</option>
-                                        <?php
-                                        foreach ($category as $category):?>
-                                            <option value="<?php echo $category->journal_category_id; ?>"><?php echo $category->journal_category_name; ?> </option>
-                                        <?php  endforeach;?>
-                                    </select>
-                                    <?php echo form_error('name'); ?>
-                                </div>
+                                        <option value="<?php echo $journaType->journal_type_id; ?>"><?php echo $journaType->journal_type_name; ?> </option>
+                                    <?php  endforeach;?>
+                                </select>
+                                <?php echo form_error('name'); ?>
                             </div>
                         </div>
-                        <div class="col-md-4 col-md-offset-2"></div>
-
-                        <!-- END Basic Elements -->
+                        <div class="form-group" id="selectshow">
+                            <label for="name1" class="col-sm-3 control-label">Journal Category</label>
+                            <div class="col-sm-9">
+                                <select name="intCatId" class="form-control" id="intCatId">
+                                    <option value="-1">Select Journal Category</option>
+                                    <?php
+                                    foreach ($category as $category):?>
+                                    <option value="<?php echo $category->journal_category_id; ?>"><?php echo $category->journal_category_name; ?> </option>
+                                <?php  endforeach;?>
+                            </select>
+                            <?php echo form_error('name'); ?>
+                        </div>
                     </div>
                 </div>
+                <div class="col-md-4 col-md-offset-2"></div>
+
+                <!-- END Basic Elements -->
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button>
-                <input type="submit" class="btn btn-primary btn-sm" value="Save Changes" />
-            </div>
-            <?php echo form_close();?>
         </div>
     </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button>
+        <input type="submit" class="btn btn-primary btn-sm" value="Save Changes" />
+    </div>
+    <?php echo form_close();?>
+</div>
+</div>
 </div>
 
 <script>
@@ -316,5 +325,5 @@
                     swal("Cancelled", "Your imaginary file is safe :)", "error");
                 }
             });
-    });*/
-</script>
+        });*/
+    </script>
